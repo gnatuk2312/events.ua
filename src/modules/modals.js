4c995a9b170ef5b8b6c216224586415190a2470f
gnatuk2312 <84677177+gnatuk2312@users.noreply.github.com>
2021-12-27 00:15:31 +0200

    JS: modal.js removed scroll

--- src/modules/modals.js
+++ src/modules/modals.js
@@ -9,14 +9,17 @@ export default class Modals {
 		this.triggers.forEach(trigger => {
 			trigger.addEventListener('click', () => {
 				this.modal.classList.remove('visually-hidden');
+				document.body.classList.add('modal-open');
 			})
 		})
 		this.close.addEventListener('click', () => {
 			this.modal.classList.add('visually-hidden');
+			document.body.classList.remove('modal-open');
 		})
 		this.modal.addEventListener('click', (e) => {
 			if (e.target === this.modal) {
 				this.modal.classList.add('visually-hidden');
+				document.body.classList.remove('modal-open');
 			}
 		})
 
