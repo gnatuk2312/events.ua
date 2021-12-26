85381d367600ffcea9fa27ccb796c68a4a8cd87e
gnatuk2312 <84677177+gnatuk2312@users.noreply.github.com>
2021-12-26 20:55:32 +0000

    Deploying to gh-pages from @ gnatuk2312/events.ua@24d74a0908b825ce312199ceccbc40fa18382a65 🚀

--- src.59a52a94.js
+++ src.59a52a94.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/events.ua/src.65a24e0d.js.map
+//# sourceMappingURL=/events.ua/src.59a52a94.js.map
