0177dc97540458cd29bdac147237ae5979f7a6b3
gnatuk2312 <84677177+gnatuk2312@users.noreply.github.com>
2021-12-27 13:58:30 +0200

    JS: modal.js calcScroll()

--- src/modules/modals.js
+++ src/modules/modals.js
@@ -5,21 +5,42 @@ export default class Modals {
 		this.close = document.querySelector(closeSelector);
 	}
 
+	calcScroll() {
+		let div = document.createElement('div');
+
+		div.style.width = "50px";
+		div.style.height = '50px';
+		div.style.overflowY = "scroll";
+		div.style.visibility = 'hidden';
+
+		document.body.appendChild(div);
+		let scrollWidth = div.offsetWidth - div.clientWidth;
+		div.remove();
+
+		return scrollWidth;
+	}
+
+
+
 	init() {
 		this.triggers.forEach(trigger => {
 			trigger.addEventListener('click', () => {
 				this.modal.classList.remove('visually-hidden');
 				document.body.classList.add('modal-open');
+				const scroll = this.calcScroll();
+				document.body.style.paddingRight = `${scroll}px`;
 			})
 		})
 		this.close.addEventListener('click', () => {
 			this.modal.classList.add('visually-hidden');
 			document.body.classList.remove('modal-open');
+			document.body.style.paddingRight = `0px`;
 		})
 		this.modal.addEventListener('click', (e) => {
 			if (e.target === this.modal) {
 				this.modal.classList.add('visually-hidden');
 				document.body.classList.remove('modal-open');
+				document.body.style.paddingRight = `0px`;
 			}
 		})
 
